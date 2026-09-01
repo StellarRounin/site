bfd92d603420371a8a5dc01028cdeaa541ea0672
Stellar Rounin <stellar.rounin+services@gmail.com>
2026-08-31 21:49:10 -0600

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment for my Hugo blog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          hugo
+          git
+        ];
+      };
+    };
+}
